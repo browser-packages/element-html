32f16b7afc1ee662d5414b2f84386a6321c39ac2
Gabriel Conceição <gabriel.conceicao@findhit.com>
2016-01-15 18:24:15 +0000

    remove dom setup

--- index.js
+++ index.js
@@ -2,15 +2,7 @@ module.exports = function (){
 
     // Set innerHTML
     if(arguments.length > 0 && typeof arguments[0] != 'undefined'){
-
-        // It means that we have to setup: before
-        if( arguments[1] === true ) Dom.setup( this, 'destroy' );
-
         this.innerHTML = arguments[0];
-
-        // It means that we have to setup: after
-        if( arguments[1] === true ) Dom.setup( this, 'initialize' );
-
         return this;
     }
 
